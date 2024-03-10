8c2bafe6a8a920e94c8e0170e2228b584cd37549
Kurtko10 <kurtkoaf@gmail.com>
2024-03-10 02:08:37 +0100

    Migración de Citas y pagos. Definir campor de las tablas

--- src/database/migrations/1709968162504-CreateCompletedAppointmentsTable.ts
+++ src/database/migrations/1709968162504-CreateCompletedAppointmentsTable.ts
@@ -1,11 +1,52 @@
-import { MigrationInterface, QueryRunner } from "typeorm";
+import { MigrationInterface, QueryRunner, Table } from "typeorm";
 
 export class CreateCompletedAppointmentsTable1709968162504 implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.createTable(new Table({
+            name: 'completed_appointments',
+            columns: [
+                {
+                    name: 'id',
+                    type: 'int',
+                    isPrimary: true,
+                    isGenerated: true,
+                    generationStrategy: 'increment',
+                },
+                {
+                    name: 'appointment_id',
+                    type: 'int',
+                },
+                {
+                    name: 'client_id',
+                    type: 'int',
+                },
+                {
+                    name: 'completed_date',
+                    type: 'datetime',
+                },
+            ],
+            foreignKeys: [
+                {                
+                    columnNames: ['appointment_id'],
+                    referencedColumnNames: ['id'],
+                    referencedTableName: 'appointments',
+                },
+                {
+                    columnNames: ['client_id'],
+                    referencedColumnNames: ['id'],
+                    referencedTableName: 'clients',
+                },
+            
+            ],
+                
+        }), 
+        true);
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
+
+        await queryRunner.dropTable('completed_appointments');
     }
 
 }
--- src/database/migrations/1709968192353-CreatePaymentsTable.ts
+++ src/database/migrations/1709968192353-CreatePaymentsTable.ts
@@ -1,11 +1,61 @@
-import { MigrationInterface, QueryRunner } from "typeorm";
+import { MigrationInterface, QueryRunner, Table } from "typeorm";
 
 export class CreatePaymentsTable1709968192353 implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner): Promise<void> {
+    
+        await queryRunner.createTable(new Table({
+            name: 'payments',
+            columns: [
+                {
+                    name: 'id',
+                    type: 'int',
+                    isPrimary: true,
+                    isGenerated: true,
+                    generationStrategy: 'increment',
+                },
+                {
+                    name: 'client_id',
+                    type: 'int',
+                },
+                {
+                    name: 'date',
+                    type: 'datetime',
+                },
+                {
+                    name: 'amount',
+                    type: 'decimal',
+                    precision: 10,
+                    scale: 2,
+                },
+                {
+                    name: 'payment_method',
+                    type: 'varchar',
+                },
+                {
+                    name: 'status',
+                    type: 'boolean',
+                    default: false,
+                },
+            ],
+            foreignKeys: [
+                {
+                    columnNames: ['client_id'],
+                    referencedColumnNames: ['id'],
+                    referencedTableName: 'clients',
+                },
+                {
+                    columnNames: ['appointment_id'],
+                    referencedColumnNames: ['id'],
+                    referencedTableName: 'appointments',
+                    onDelete: 'CASCADE',
+                }
+            ]
+        }), true);
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.dropTable('payments');
     }
 
 }
